fc99188a0683cf4b3c76980f02b19e7980099e21
0214sh7 <0214sh7@gmail.com>
2022-05-20 20:42:29 +0900

    test2

---
+++ works/counter.js
@@ -0,0 +1,18 @@
+let count = 0
+const countUp = () => {
+  count++
+  const countElement = document.querySelector('#count')
+  countElement.innerText = '回数: ' + count
+}
+
+const countDown = () => {
+  count--
+  const countElement = document.querySelector('#count')
+  countElement.innerText = '回数: ' + count
+}
+
+const countReset = () => {
+  count = 0
+  const countElement = document.querySelector('#count')
+  countElement.innerText = '回数: ' + 'ひみつ❤'
+}
